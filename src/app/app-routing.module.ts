de98b2a94249b1b4ddf884f7b315d5a67bfb44ea
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2023-11-24 12:54:10 -0300

    Aula17

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path: 'directives', component: DirectivesComponent},
+  {path: 'if-render', component: IfRenderComponent},
+  {path: 'events', component: EventosComponent},
+  {path: 'emitter', component: EmitterComponent},
+  {path: 'list', component: ListRenderComponent},
+  {path: 'pipes', component: PipesComponent},
+  {path: 'two-way-binding', component: TwoWayBindingComponent}
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
